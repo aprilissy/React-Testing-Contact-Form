380c1555316fea780015adc6c2fcc170a5792ab7
April Darger <april.m.darger@gmail.com>
2020-10-29 16:02:08 -0600

    Added id field to jsx, added await waitFor() to test. Got tests working after zoom call with Warren Longmire.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,27 +1,42 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import ContactForm from './ContactForm'
 
 test('renders ContactForm.js to the DOM without errors', () => {
   render(<ContactForm />)
 })
 
-test('user can fill out and submit form', () => {
-  render(<ContactForm />)
+test('user can fill out and submit form',async () => {
 
   //Arrange: render component
   render(<ContactForm />);
 
   //Act:
   //1. get access to form fields
-  const firstNameInput = screen.getAllByLabelText(/first name*/i)
-  const lastNameInput = screen.getAllByLabelText(/last name*/i)
-  const emailInput = screen.getAllByLabelText(/email/i)
-  const messageInput = screen.getAllByLabelText(/message/i)
+  const firstNameInput = screen.getByLabelText(/first name*/i)
+  const lastNameInput = screen.getByLabelText(/last name*/i)
+  const emailInput = screen.getByLabelText(/email*/i)
+  const messageInput = screen.getByLabelText(/message/i)
 
   //2. add text to our fields
+  fireEvent.change(firstNameInput, { target:{ value:'Ap', name:'firstName'}});
+  fireEvent.change(lastNameInput, { target:{ value:'Darger', name:'lastName'}});
+  fireEvent.change(emailInput, { target:{ value:'a@a.com', name:'email'}});
+  fireEvent.change(messageInput, { target:{ value:'abc and 123', name:'message'}});
 
   //3. get access to and click our button
+  const button = screen.getByTestId('button')
+  fireEvent.click(button)
 
   //Assert Check that text is on screen
+  await waitFor(() => {    
+    const firstNameText = screen.getByText(/ap/i)
+    expect(firstNameText).toBeTruthy()
+    const lastNameInput = screen.getByText(/darger/i)
+    expect(lastNameInput).toBeTruthy()
+    const emailInput = screen.getByText(/a@a.com/i)
+    expect(emailInput).toBeTruthy()
+    const messageInput = screen.getByText(/abc and 123/i)
+    expect(messageInput).toBeTruthy()
+  });
 })
